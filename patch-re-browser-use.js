cf1ba2b6d02d60dacd578d5f00f3da3b632b0cdd
pvm14 <you@example.com>
2025-07-06 02:41:31 +0200

    Trying to patch 0.4.4 version ...

--- patch-re-browser-use.js
+++ patch-re-browser-use.js
@@ -1,7 +1,7 @@
 import { Project, Node, SyntaxKind, IndentationText } from 'ts-morph';
 import path from 'path';
 const FILES_LOCATION_PREFIX = "browser-use/"
-const filePath = path.resolve(`${FILES_LOCATION_PREFIX}browser_use/dom/buildDomTree.js`);
+const filePath = path.resolve(`${FILES_LOCATION_PREFIX}browser_use/dom/dom_tree/index.js`);
 
 // Initialize a ts-morph project. It's configured to handle JavaScript files.
 const project = new Project({
