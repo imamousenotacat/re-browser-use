415b3a5193361eca509975faa6e8595362171a35
imamousenotacat <216721444+imamousenotacat@users.noreply.github.com>
2025-08-30 22:31:01 +0200

    python examples\nopecha_cloudflare_no_playwright.py works ...

--- examples/nopecha_cloudflare_no_playwright.py
+++ examples/nopecha_cloudflare_no_playwright.py
@@ -10,8 +10,8 @@ load_dotenv()
 async def main():
   agent = Agent(
     task=(
-      "Go to https://nopecha.com/demo/cloudflare, wait for the verification checkbox to appear, click it once, and wait for 10 seconds."
-      "That’s all. If you get redirected, don’t worry."
+    "Go to https://nopecha.com/demo/cloudflare, and always wait 10 seconds for the verification checkbox to appear."
+    "Once it appears, click it once, and wait 5 more seconds. That’s all. Your job is done. Don't check anything. If you get redirected, don’t worry."
     ),
     llm=ChatGoogle(model="gemini-2.5-flash-lite"),
     browser_session=BrowserSession(
