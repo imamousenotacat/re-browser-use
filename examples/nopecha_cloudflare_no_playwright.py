edf9a55f81cb36b639be5103dbd2cef67b73b6f5
imamousenotacat <216721444+imamousenotacat@users.noreply.github.com>
2025-09-15 16:56:21 +0200

    Working a little bit on marketing  ...

--- examples/nopecha_cloudflare_no_playwright.py
+++ examples/nopecha_cloudflare_no_playwright.py
@@ -18,10 +18,10 @@ async def main():
     browser_session=BrowserSession(
       browser_profile=BrowserProfile(
         headless=False,
-        # Both options below are needed to pass Cloudflare challenge
+        highlight_elements=True,
+        # Both options below need to be set to these values to pass Cloudflare challenge
         disable_security=False,
-        cross_origin_iframes=True,
-        highlight_elements=True
+        cross_origin_iframes=True
       )
     )
   )
