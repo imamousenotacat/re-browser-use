859b7301f8d24ebd5566ab64752da08270bca02b
imamousenotacat <216721444+imamousenotacat@users.noreply.github.com>
2025-10-31 17:59:08 +0100

    DomViewsTransformer not needed anymore, the bug was fixed. They relocated the tests files ...

--- patch-re-browser-use.py
+++ patch-re-browser-use.py
@@ -12,7 +12,6 @@ from libcst_transformers.simple_server_transformer import SimpleServerTransforme
 from libcst_transformers.default_action_watchdog_transformer import DefaultActionWatchdogTransformer
 # from libcst_transformers.highlights_transformer import HighlightsTransformer
 from libcst_transformers.dom_serializer_transformer import DomSerializerTransformer
-from libcst_transformers.dom_views_transformer import DomViewsTransformer
 from libcst_transformers.screenshot_watchdog_transformer import ScreenshotWatchdogTransformer
 from libcst_transformers.aboutblank_watchdog_transformer import AboutBlankWatchdogTransformer
 from libcst_transformers.local_browser_watchdog_transformer import LocalBrowserWatchdogTransformer
@@ -51,7 +50,7 @@ def patch_python_file(file_path: str, transformer: cst.CSTTransformer):
 # TODO: MOU14 THESE EXECUTIONS AREN'T IDEMPOTENT FOR THE MOMENT ...
 # Pre 0.6.1 transformers still valid
 patch_python_file("tests/ci/conftest.py", ConfTestTransformer())
-patch_python_file("tests/ci/test_radio_buttons.py", TestRadioButtonsTransformer())
+patch_python_file("tests/ci/interactions/test_radio_buttons.py", TestRadioButtonsTransformer())
 patch_python_file("tests/ci/evaluate_tasks.py", EvaluateTaskTransformer())
 patch_python_file("browser_use/llm/google/chat.py", ChatGoogleTransformer())
 patch_python_file("browser_use/mcp/server.py", MCPServerTransformer())
@@ -62,7 +61,6 @@ patch_python_file("browser_use/browser/watchdogs/default_action_watchdog.py", De
 patch_python_file("browser_use/browser/session.py", BrowserSessionTransformer())
 patch_python_file("browser_use/dom/serializer/serializer.py", DomSerializerTransformer())
 patch_python_file("browser_use/dom/service.py", DomServiceTransformer())
-patch_python_file("browser_use/dom/views.py", DomViewsTransformer())
 patch_python_file("browser_use/browser/watchdogs/screenshot_watchdog.py", ScreenshotWatchdogTransformer())
 patch_python_file("browser_use/browser/watchdogs/aboutblank_watchdog.py", AboutBlankWatchdogTransformer())
 patch_python_file("browser_use/browser/watchdogs/local_browser_watchdog.py", LocalBrowserWatchdogTransformer())
@@ -75,7 +73,7 @@ with open_file("pyproject.toml") as f:
 deps = doc["project"]["dependencies"]
 
 # Add the dependency to the library enabling real clicks ...
-deps.append("re-cdp-patches>=0.9.2")
+deps.append("re-cdp-patches>=0.9.1")
 
 # and remove the required-environments key from [tool.uv]
 if "tool" in doc and "uv" in doc["tool"]:
